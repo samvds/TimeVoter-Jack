3f54ed8980ad3e248d88fd7d37e4d741c1930174
JackNytely <phantasystarunive12@gmail.com>
2022-12-15 19:37:07 +0200

    JackNytely Initial Update

--- hooks/OnCommand.cs
+++ hooks/OnCommand.cs
@@ -15,7 +15,29 @@ namespace TimeVoter.hooks
         //Setup the Constructor
         public static void Run(Player player, string cmd, string[] args)
         {
+            if (cmd.ToLower() == "day")
+            {
+                Commands.day.Run(player, cmd, args);
+            }
 
+            if (cmd.ToLower() == "night")
+            {
+                Commands.night.Run(player, cmd, args);
+            }
+
+            if (cmd.ToLower() == "settime")
+            {
+                Commands.settime.Run(player, cmd, args);
+            }
+
+            if (cmd.ToLower() == "time")
+            {
+                Commands.time.Run(player, cmd, args);
+
+            }
+
+            //// NEED TO FIX BELOW... COMMANDS DO NOT RUN \\\\
+            /*
             //Get the Command
             Type CommandClass = Type.GetType($"Timevoter.Commands.{cmd.ToLower()}");
 
@@ -27,6 +49,7 @@ namespace TimeVoter.hooks
 
             //Run the Command
             CommandClass.GetMethod("Run").Invoke(null, parametersArray);
+            */
         }
     }
 }
